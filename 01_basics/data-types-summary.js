ac4aeb448b8b52e6b6e99c39fa6130172e80c2ae
Abhishek Painuli <abhishekpainuliabhi77@gmail.com>
2024-04-07 17:15:09 +0530

    Symbol Data Type Expaination

---
+++ 01_basics/data-types-summary.js
@@ -0,0 +1,20 @@
+// Primitive Data type - 7 Types :-
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// BigInt
+
+const id = Symbol("123");
+const anotherId = Symbol("123")
+
+console.table([id, anotherId])
+console.log(id == anotherId);
+
+
+// Reference (Non Premitive Data type) :-
+// Array
+// Objects
+// Functions
