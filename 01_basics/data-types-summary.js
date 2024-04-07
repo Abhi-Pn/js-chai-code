84965eddc5a3fe2e4ac200f02a9708bac85099ab
Abhishek Painuli <abhishekpainuliabhi77@gmail.com>
2024-04-07 17:22:20 +0530

    Big Integer Added

--- 01_basics/data-types-summary.js
+++ 01_basics/data-types-summary.js
@@ -11,7 +11,10 @@ const id = Symbol("123");
 const anotherId = Symbol("123")
 
 console.table([id, anotherId])
-console.log(id == anotherId);
+console.log(id === anotherId); 
+
+let bigNumber = 4964930857688756434343434n
+console.log(typeof bigNumber);
 
 
 // Reference (Non Premitive Data type) :-
