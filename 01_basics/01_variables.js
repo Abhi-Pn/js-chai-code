446d1b1762b8836a19123096f8bd67df368a8677
Abhishek Painuli <114172758+Abhi-Pn@users.noreply.github.com>
2024-04-07 12:58:43 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,37 @@
+const accountId = 144553 ;
+let accountEmail = "abhishek@gmail.com" ;
+var accountPassword = "12345" ;
+accountCity = "Delhi" ;
+
+//console.table(typeof[accountId, accountEmail, accountPassword, accountCity])
+
+let logIn = "Abhishek";
+let booleanLogIn = Boolean(logIn)
+//console.log(booleanLogIn)
+
+let someString = 12345;
+let stringNumber = String(someString);
+//console.log(someString);\
+
+let value = 7;
+let negValue = -value;
+//console.log(negValue);
+
+let str1 = "Abhishek";
+let str2 = " Painuli";
+let str3 = str1 + str2;
+//console.log(str3);
+
+/*console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" + 2);*/
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+
+
+console.log("2" == 2);
+console.log("2" === 2);
