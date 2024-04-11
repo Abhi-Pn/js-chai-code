b3a886ab9560ebc404eb6a6f322b008a7e127c38
Abhishek Painuli <abhishekpainuliabhi77@gmail.com>
2024-04-11 22:57:52 +0530

    Date Methods Added

---
+++ 01_basics/06_dates.js
@@ -0,0 +1,33 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(typeof myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+
+let myCreatedDate = new Date(2024, 3, 12); // In Java Script Months starts from "0". i.e January = 0, Febuary = 1, March = 2,.....
+console.log(myCreatedDate);
+console.log(myCreatedDate.toDateString());
+
+let myCreatedDate2 = new Date(2024, 3, 12, 7, 27);
+console.log(myCreatedDate2.toLocaleString());
+
+let myCreatedDate3 = new Date("2024-04-11");
+console.log(myCreatedDate3.toLocaleDateString());
+
+// Time Stamps----------------------------
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+console.log(Date.now()/1000);
+console.log(Math.floor(Date.now()/1000));
+
+
+let myCreatedDate4 = new Date("2024-04-11");
+console.log(myCreatedDate4.getTime());
+
+let myDates = new Date();
+console.log(newDates);
+
