17ed2fd7965c37f868da9a33c31d0cef80938e6b
Abhishek Painuli <abhishekpainuliabhi77@gmail.com>
2024-04-27 00:41:40 +0530

    Array methods added

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,15 @@
 // array
-const myArr = [9, 3, 6, 8, 6, 2, 4]
-const myFriend = ["Akash", "Gaurav", "Chandan", "Pulkit", "Vinay"]
+const myArr = [9, 3, 6, 8, 6, 2, 4];
+const myFriend = ["Akash", "Gaurav", "Chandan", "Pulkit", "Vinay"];
+const myArr2 = new Array("Mango", "Banana", "Kiwi", "Watermelon");
 
-console.log(myArr[0], myFriend[3]);
+console.log(myArr[0], myFriend[3], myArr2[3]);
+
+// Array Methods--------------------
+
+myArr2.push("Pineapple"); // To push new value or data
+myArr2.push("Orange"); // To push new value or data
+
+myArr2.pop(); // To remove last value or data
+myArr2.unshift("Fruits"); // To add value or data at the start
+console.log(myArr2);
